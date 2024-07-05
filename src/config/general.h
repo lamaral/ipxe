0cd23267ebdc1f07ad17a088cc29ccc053c5ede0
Luiz Amaral <luiz.amaral@innogames.com>
2024-07-05 16:59:42 +0200

    Disabled lots of things

--- src/config/general.h
+++ src/config/general.h
@@ -34,7 +34,7 @@ FILE_LICENCE ( GPL2_OR_LATER_OR_UBDL );
  *
  */
 
-#define	NET_PROTO_IPV4		/* IPv4 protocol */
+#undef	NET_PROTO_IPV4		/* IPv4 protocol */
 #define NET_PROTO_IPV6	/* IPv6 protocol */
 #undef	NET_PROTO_FCOE		/* Fibre Channel over Ethernet protocol */
 #define	NET_PROTO_STP		/* Spanning Tree protocol */
@@ -55,33 +55,33 @@ FILE_LICENCE ( GPL2_OR_LATER_OR_UBDL );
  */
 
 #define	DOWNLOAD_PROTO_TFTP	/* Trivial File Transfer Protocol */
-#define	DOWNLOAD_PROTO_HTTP	/* Hypertext Transfer Protocol */
+#undef	DOWNLOAD_PROTO_HTTP	/* Hypertext Transfer Protocol */
 #undef	DOWNLOAD_PROTO_HTTPS	/* Secure Hypertext Transfer Protocol */
 #undef	DOWNLOAD_PROTO_FTP	/* File Transfer Protocol */
 #undef	DOWNLOAD_PROTO_SLAM	/* Scalable Local Area Multicast */
 #undef	DOWNLOAD_PROTO_NFS	/* Network File System Protocol */
-//#undef DOWNLOAD_PROTO_FILE	/* Local filesystem access */
+#undef DOWNLOAD_PROTO_FILE	/* Local filesystem access */
 
 /*
  * SAN boot protocols
  *
  */
 
-//#undef	SANBOOT_PROTO_ISCSI	/* iSCSI protocol */
-//#undef	SANBOOT_PROTO_AOE	/* AoE protocol */
-//#undef	SANBOOT_PROTO_IB_SRP	/* Infiniband SCSI RDMA protocol */
-//#undef	SANBOOT_PROTO_FCP	/* Fibre Channel protocol */
-//#undef	SANBOOT_PROTO_HTTP	/* HTTP SAN protocol */
+#undef	SANBOOT_PROTO_ISCSI	/* iSCSI protocol */
+#undef	SANBOOT_PROTO_AOE	/* AoE protocol */
+#undef	SANBOOT_PROTO_IB_SRP	/* Infiniband SCSI RDMA protocol */
+#undef	SANBOOT_PROTO_FCP	/* Fibre Channel protocol */
+#undef	SANBOOT_PROTO_HTTP	/* HTTP SAN protocol */
 
 /*
  * HTTP extensions
  *
  */
-#define HTTP_AUTH_BASIC		/* Basic authentication */
-#define HTTP_AUTH_DIGEST	/* Digest authentication */
-//#define HTTP_AUTH_NTLM	/* NTLM authentication */
-//#define HTTP_ENC_PEERDIST	/* PeerDist content encoding */
-//#define HTTP_HACK_GCE		/* Google Compute Engine hacks */
+#undef HTTP_AUTH_BASIC		/* Basic authentication */
+#undef HTTP_AUTH_DIGEST	/* Digest authentication */
+#undef HTTP_AUTH_NTLM	/* NTLM authentication */
+#undef HTTP_ENC_PEERDIST	/* PeerDist content encoding */
+#undef HTTP_HACK_GCE		/* Google Compute Engine hacks */
 
 /*
  * 802.11 cryptosystems and handshaking protocols
@@ -137,24 +137,24 @@ FILE_LICENCE ( GPL2_OR_LATER_OR_UBDL );
 #define	NVO_CMD			/* Non-volatile option storage commands */
 #define	CONFIG_CMD		/* Option configuration console */
 #define	IFMGMT_CMD		/* Interface management commands */
-#define	IWMGMT_CMD		/* Wireless interface management commands */
-#define IBMGMT_CMD		/* Infiniband management commands */
-#define FCMGMT_CMD		/* Fibre Channel management commands */
-#define	ROUTE_CMD		/* Routing table management commands */
-#define IMAGE_CMD		/* Image management commands */
+//#define	IWMGMT_CMD		/* Wireless interface management commands */
+//#define IBMGMT_CMD		/* Infiniband management commands */
+//#define FCMGMT_CMD		/* Fibre Channel management commands */
+//#define	ROUTE_CMD		/* Routing table management commands */
+//#define IMAGE_CMD		/* Image management commands */
 #define DHCP_CMD		/* DHCP management commands */
-#define SANBOOT_CMD		/* SAN boot commands */
-#define MENU_CMD		/* Menu commands */
-#define FORM_CMD		/* Form commands */
-#define LOGIN_CMD		/* Login command */
-#define SYNC_CMD		/* Sync command */
-#define SHELL_CMD		/* Shell command */
+//#define SANBOOT_CMD		/* SAN boot commands */
+//#define MENU_CMD		/* Menu commands */
+//#define FORM_CMD		/* Form commands */
+//#define LOGIN_CMD		/* Login command */
+//#define SYNC_CMD		/* Sync command */
+//#define SHELL_CMD		/* Shell command */
 //#define NSLOOKUP_CMD		/* DNS resolving command */
 //#define TIME_CMD		/* Time commands */
 //#define DIGEST_CMD		/* Image crypto digest commands */
 //#define LOTEST_CMD		/* Loopback testing commands */
-#define VLAN_CMD		/* VLAN commands */
-//#define PXE_CMD		/* PXE commands */
+//#define VLAN_CMD		/* VLAN commands */
+#define PXE_CMD		/* PXE commands */
 #define REBOOT_CMD		/* Reboot command */
 #define POWEROFF_CMD		/* Power off command */
 //#define IMAGE_TRUST_CMD	/* Image trust management commands */
@@ -168,8 +168,8 @@ FILE_LICENCE ( GPL2_OR_LATER_OR_UBDL );
 //#define NTP_CMD		/* NTP commands */
 //#define CERT_CMD		/* Certificate management commands */
 //#define IMAGE_MEM_CMD		/* Read memory command */
-#define IMAGE_ARCHIVE_CMD	/* Archive image management commands */
-#define SHIM_CMD		/* EFI shim command (or dummy command) */
+//#define IMAGE_ARCHIVE_CMD	/* Archive image management commands */
+//#define SHIM_CMD		/* EFI shim command (or dummy command) */
 
 /*
  * ROM-specific options
@@ -209,9 +209,9 @@ FILE_LICENCE ( GPL2_OR_LATER_OR_UBDL );
 #undef	GDBUDP			/* Remote GDB debugging over UDP
 				 * (both may be set) */
 //#define EFI_DOWNGRADE_UX	/* Downgrade UEFI user experience */
-#define	TIVOLI_VMM_WORKAROUND	/* Work around the Tivoli VMM's garbling of SSE
-				 * registers when iPXE traps to it due to
-				 * privileged instructions */
+//#define	TIVOLI_VMM_WORKAROUND	/* Work around the Tivoli VMM's garbling of SSE
+//				 * registers when iPXE traps to it due to
+//				 * privileged instructions */
 
 #include <config/named.h>
 #include NAMED_CONFIG(general.h)
